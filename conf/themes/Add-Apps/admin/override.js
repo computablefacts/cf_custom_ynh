374a25055fc29b0fe80e69097ab0c903d0de26b6
Patrick Brisacier (ynh01) <pbrisacier@mncc.fr>
2023-09-07 11:12:29 +0000

    AddApps - Improve logo handling in admin panel.

--- conf/themes/Add-Apps/admin/override.js
+++ conf/themes/Add-Apps/admin/override.js
@@ -2,26 +2,16 @@
 
   'use strict'
 
-  function waitForElm(selector) {
-    return new Promise(resolve => {
-        if (document.querySelector(selector)) {
-            return resolve(document.querySelector(selector));
-        }
-        const observer = new MutationObserver(mutations => {
-            if (document.querySelector(selector)) {
-                resolve(document.querySelector(selector));
-                observer.disconnect();
-            }
-        });
-        observer.observe(document.body, {
-            childList: true,
-            subtree: true
-        });
-    });
-  }
-
-  waitForElm('div#app header nav a span img').then(img => {
-    img.src='/yunohost/admin/logo.png';
+  const observer = new MutationObserver(mutations => {
+    if (document.querySelector('div#app header nav a span img')) {
+      const image = document.querySelector('div#app header nav a span img');
+      image.src='/yunohost/admin/logo.png';
+    }
+  });
+  
+  observer.observe(document.body, {
+    childList: true,
+    subtree: true
   });
 
 })();
